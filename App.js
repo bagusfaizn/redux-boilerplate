c0b7de3f5aea140276d49014bf57f0726df16554
isakh <rafiisakh@gmail.com>
2018-03-22 21:18:30 +0700

    add dependency

--- App.js
+++ App.js
@@ -8,13 +8,14 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 
 import store from './app/store'
+import Main from './app/index'
 import Home from './app/components/home'
 
 export default class App extends Component {
   render() {
     return (
         <Provider store={store}>
-          <Home />
+          <Main />
         </Provider>
     )
   }
